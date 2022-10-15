e95365ef9645f8c9a49fed4427e5d328e5ec00d6
bs f <304446350@qq.com>
2022-10-15 19:10:35 +0800

    login review

--- seckill/src/main/java/com/boshen/seckill/utils/MD5Util.java
+++ seckill/src/main/java/com/boshen/seckill/utils/MD5Util.java
@@ -2,16 +2,18 @@ package com.boshen.seckill.utils;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MD5Util {
     public static String md5(String src){
         return DigestUtils.md5Hex(src);
     }
 
     private static final String salt="la2b3c4d";
-
+//salt.charAt(0) + salt.charAt(2) + salt.charAt(3) + salt.charAt(5) + inputPass
     public static String inputPassToFromPass(String inputPass){
-        String str = salt.charAt(0)+salt.charAt(2)+salt.charAt(3)+salt.charAt(5)+inputPass;
+        String str = inputPass;
         return md5(str);
     }
     public static String formPassToDBPass(String formPass, String salt) {
@@ -23,5 +25,10 @@ public class MD5Util {
         String fromPass = inputPassToFromPass(inputPass);
         String dbPass = formPassToDBPass(fromPass, salt);
         return dbPass;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(inputPassToFromPass("123456"));
+        System.out.println("e4332af42e3640df495b82f8a68b198c".equals(""));
     }
 }
