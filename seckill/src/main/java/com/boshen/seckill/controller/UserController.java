795474e978b2f6cb49160f8d9d62d8f476546f1b
bs f <304446350@qq.com>
2022-10-22 01:06:36 +0800

    mq basic

--- seckill/src/main/java/com/boshen/seckill/controller/UserController.java
+++ seckill/src/main/java/com/boshen/seckill/controller/UserController.java
@@ -2,7 +2,9 @@ package com.boshen.seckill.controller;
 
 
 import com.boshen.seckill.pojo.User;
+import com.boshen.seckill.rabbitmq.MQSender;
 import com.boshen.seckill.vo.RespBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +21,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private MQSender sender;
+
+
     @RequestMapping("/info")
     @ResponseBody
     public RespBean info(User user){
         return RespBean.success(user);
     }
+
+
+    @RequestMapping("/mq")
+    @ResponseBody
+    public void mq(){
+        sender.send("hello");
+    }
+
 
 }
